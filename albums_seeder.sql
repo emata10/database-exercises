198234c050718ed7e013160a84d3e54a4da78737
emata10 <130502916+emata10@users.noreply.github.com>
2023-07-12 17:12:01 -0600

    where_exercises

--- albums_seeder.sql
+++ albums_seeder.sql
@@ -1,6 +1,5 @@
 USE codeup_test_db;
-DROP TABLE IF EXISTS albums;
-
+TRUNCATE albums;
 CREATE TABLE albums(
         id INT UNSIGNED NOT NULL AUTO_INCREMENT,
         artist VARCHAR(50),
---
+++ delete_exercises.sql
@@ -0,0 +1,4 @@
+SELECT name, release_date FROM albums WHERE release_date > 1991;
+
+DELETE FROM albums where release_date > 1991;
+SELECT genre
